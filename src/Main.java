9352b53af211f3a4698dc0a667dd81fe96a7b5df
Thomas <thomas.dulcamara@alumni.univ-avignon.fr>
2019-12-10 15:45:11 +0100

    Debut des cases

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		Case testCase = new Case(2, true, null, 3, 4);
+		testCase.affichage();
 	}
+	
+	
 }
