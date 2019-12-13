47e52218fa5f485d6347d563cacbc42cd25a85f3
loic <loic.guilmain@alumni.univ-avignon.fr>
2019-12-13 17:41:00 +0100

    all find Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,22 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		Case testCase = new Case(2, true, null, 3, 4);
-		testCase.affichage();
-
-
-
-
-
-
-
 		//Test LOIC
 		int cote=3;
 		Case[][] tableauCase = new Case[cote][cote];
+		Case tmpCase;
 		for(int i=0; i<cote; i++)
 		{
 			for(int j=0; j<cote; j++)
 			{
 				int x=i;
 				int y=j;
-				Case tmpCase = new Case(2, true, null, x, y);
+				tmpCase = new Case(2, false, null, x, y);
 				tableauCase[i][j]=tmpCase;
 			}
 		}
-
 		Plateau plateau=new Plateau(cote,tableauCase);
-		System.out.print(plateau.find(0,0)+"\n");
-		System.out.print(plateau.find(0,1)+"\n");
+		plateau.affichage();
+
 	}
 	
 	
--- src/Plateau.java
+++ src/Plateau.java
@@ -12,6 +12,14 @@ public class Plateau {
     }
 
     public void affichage() {
+        for(int i=0; i<COTE; i++)
+        {
+            for(int j=0; j<COTE; j++)
+            {
+                tableauCase[i][j].affichage();
+            }
+            System.out.println();
+        }
     }
 
     public boolean sauvegarde() {
@@ -34,7 +42,7 @@ public class Plateau {
         return plateau;
     }
 
-    public Case find(int x,int y){
+    private Case find(int x,int y){
         if((x<0 || y<0) || (x>COTE || y>COTE)){
             return null;
         }
@@ -52,11 +60,11 @@ public class Plateau {
             return null;
         }
     }
-    public Case findTop(Case positionActuelle){
+    private Case findTop(Case positionActuelle){
         if(positionActuelle!=null){
             int tmpX=positionActuelle.getX()-1;
             int tmpY=positionActuelle.getY();
-            if(tmpX<0){
+            if(tmpX<0 || tmpX >COTE){
                 return null;
             }
             else{
@@ -65,8 +73,74 @@ public class Plateau {
         }
         return null;
     }
+    private Case findBot(Case positionActuelle){
+        if(positionActuelle!=null){
+            int tmpX=positionActuelle.getX()+1;
+            int tmpY=positionActuelle.getY();
+            if(tmpX<0 || tmpX >COTE){
+                return null;
+            }
+            else{
+                return find(tmpX,tmpY);
+            }
+        }
+        return null;
+    }
+    private Case findRight(Case positionActuelle){
+        if(positionActuelle!=null){
+            int tmpX=positionActuelle.getX();
+            int tmpY=positionActuelle.getY()+1;
+            if(tmpY<0 || tmpY >COTE){
+                return null;
+            }
+            else{
+                return find(tmpX,tmpY);
+            }
+        }
+        return null;
+    }
+    private Case findLeft(Case positionActuelle){
+        if(positionActuelle!=null){
+            int tmpX=positionActuelle.getX();
+            int tmpY=positionActuelle.getY()-1;
+            if(tmpY<0 || tmpY >COTE){
+                return null;
+            }
+            else{
+                return find(tmpX,tmpY);
+            }
+        }
+        return null;
+    }
+    private Case findTopLeft(Case positionActuelle){
+        Case tmpCase=findTop(positionActuelle);
+        tmpCase=findLeft(tmpCase);
+        return tmpCase;
+    }
+    private Case findTopRight(Case positionActuelle){
+        Case tmpCase=findTop(positionActuelle);
+        tmpCase=findRight(tmpCase);
+        return tmpCase;
+    }
+    private Case findBotLeft(Case positionActuelle){
+        Case tmpCase=findBot(positionActuelle);
+        tmpCase=findLeft(tmpCase);
+        return tmpCase;
+    }
+    private Case findBotRight(Case positionActuelle){
+        Case tmpCase=findBot(positionActuelle);
+        tmpCase=findRight(tmpCase);
+        return tmpCase;
+    }
 
     public boolean verificationDeplacement(String evenement,Corsaire joueur){
+        switch(evenement){
+            case "top":
+                Case tmpCase = findTop(joueur.getEmplacement());
+                if(tmpCase==null){
+                    return false;
+                }
+        }
         return false;
     }
 }
